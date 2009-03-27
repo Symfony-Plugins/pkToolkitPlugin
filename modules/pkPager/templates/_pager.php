b8d9f97cc9eca52b74e011da00a8af00e0914dda
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-03-27 18:20:16 -0500

    pkPager module (currently just a home for a partial really; it's a convenience for producing attractive search results with sfDoctrinePager).
    pkZendSearch class, which cuts down on code duplication when implementing
    zend search indexing for a new datatype.
    
    
    
    git-svn-id: http://svn.symfony-project.com/plugins/pkToolkitPlugin/trunk@16682 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ modules/pkPager/templates/_pager.php
@@ -0,0 +1,35 @@
+<?php # Not really a new pager - just a well-styled partial for use with ?>
+<?php # sfPager implementations (i.e. sfDoctrinePager). ?>
+<?php # Pass $pager and $pagerUrl. $pagerUrl can have other parameters in ?>
+<?php # the query string already. This partial will add the page parameter ?>
+<?php # as appropriate. ?>
+
+<div class="pk_pager_navigation">
+
+	<?php if ($pager->getPage() == 1):?>
+		<span class="pk_pager_navigation_image pk_pager_navigation_first pk_pager_navigation_disabled">First Page</span>	
+	  <span class="pk_pager_navigation_image pk_pager_navigation_previous pk_pager_navigation_disabled">Previous Page</span>
+	<?php else: ?>
+		<a href="<?php echo url_for(pkUrl::addParams($pagerUrl, array("page" => 1))) ?>" class="pk_pager_navigation_image pk_pager_navigation_first">First Page</a>
+  	<a href="<?php echo url_for(pkUrl::addParams($pagerUrl, array("page" => $pager->getPreviousPage()))) ?>" class="pk_pager_navigation_image pk_pager_navigation_previous">Previous Page</a>
+	<?php endif ?>
+
+  <?php // TBB: $pager->getLinks() generates an error message if there ?>
+  <?php // are zero results ?>
+  <?php if ($pager->getNbResults() > 0): ?>
+    <?php foreach ($pager->getLinks() as $page): ?>
+      <?php if ($page == $pager->getPage()): ?>
+        <span class="pk_page_navigation_number pk_pager_navigation_disabled"><?php echo $page ?></span>
+      <?php else: ?>
+        <a href="<?php echo url_for(pkUrl::addParams($pagerUrl, array("page" => $page))) ?>" class="pk_page_navigation_number"><?php echo $page ?></a>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  <?php endif ?>
+	<?php if ($pager->getPage() >= $pager->getLastPage()):?>
+	  <span class="pk_pager_navigation_image pk_pager_navigation_next pk_pager_navigation_disabled">Next Page</span>
+		<span class="pk_pager_navigation_image pk_pager_navigation_last pk_pager_navigation_disabled">Last Page</span>	
+	<?php else: ?>
+	  <a href="<?php echo url_for(pkUrl::addParams($pagerUrl, array("page" => $pager->getNextPage()))) ?>" class="pk_pager_navigation_image pk_pager_navigation_next">Next Page</a>
+  	<a href="<?php echo url_for(pkUrl::addParams($pagerUrl, array("page" => $pager->getLastPage()))) ?>" class="pk_pager_navigation_image pk_pager_navigation_last">Last Page</a>
+	<?php endif ?>
+</div>
