aabb940e4345e16b8b7e36d9314cac9a4c0e2ab5
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-05-26 10:47:45 -0500

    Don't display pager when not needed
    git-svn-id: http://svn.symfony-project.com/plugins/pkToolkitPlugin/trunk@18650 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkPager/templates/_pager.php
+++ modules/pkPager/templates/_pager.php
@@ -4,25 +4,17 @@
 <?php # the query string already. This partial will add the page parameter ?>
 <?php # as appropriate. ?>
 
-<div class="pk_pager_navigation">
-  <?php // Work around various ways the pagers don't work when there is ?>
-  <?php // no absolute need for pagination, since we like to display ?>
-  <?php // the pager anyway ?>
-  <?php if ($pager->getPage() == 0): ?>
-    <?php $pager->setPage(1) ?>
-  <?php endif ?>
-	<?php if ($pager->getPage() == 1):?>
-		<span class="pk_pager_navigation_image pk_pager_navigation_first pk_pager_navigation_disabled">First Page</span>	
-	  <span class="pk_pager_navigation_image pk_pager_navigation_previous pk_pager_navigation_disabled">Previous Page</span>
-	<?php else: ?>
-		<a href="<?php echo url_for(pkUrl::addParams($pagerUrl, array("page" => 1))) ?>" class="pk_pager_navigation_image pk_pager_navigation_first">First Page</a>
-  	<a href="<?php echo url_for(pkUrl::addParams($pagerUrl, array("page" => $pager->getPreviousPage()))) ?>" class="pk_pager_navigation_image pk_pager_navigation_previous">Previous Page</a>
-	<?php endif ?>
+<?php // Turns out we don't want to display the pager when there's only one page after all ?>
+<?php if ($pager->haveToPaginate()): ?>
+  <div class="pk_pager_navigation">
+  	<?php if ($pager->getPage() == 1):?>
+  		<span class="pk_pager_navigation_image pk_pager_navigation_first pk_pager_navigation_disabled">First Page</span>	
+  	  <span class="pk_pager_navigation_image pk_pager_navigation_previous pk_pager_navigation_disabled">Previous Page</span>
+  	<?php else: ?>
+  		<a href="<?php echo url_for(pkUrl::addParams($pagerUrl, array("page" => 1))) ?>" class="pk_pager_navigation_image pk_pager_navigation_first">First Page</a>
+    	<a href="<?php echo url_for(pkUrl::addParams($pagerUrl, array("page" => $pager->getPreviousPage()))) ?>" class="pk_pager_navigation_image pk_pager_navigation_previous">Previous Page</a>
+  	<?php endif ?>
 
-  <?php // TBB: $pager->getLinks() generates an error message if there ?>
-  <?php // is only one page sigh ?>
-  <?php // This is a nasty workaround ?>
-  <?php if ($pager->getNbResults() > 5): ?>
     <?php foreach ($pager->getLinks() as $page): ?>
       <?php if ($page == $pager->getPage()): ?>
         <span class="pk_page_navigation_number pk_pager_navigation_disabled"><?php echo $page ?></span>
@@ -30,12 +22,12 @@
         <a href="<?php echo url_for(pkUrl::addParams($pagerUrl, array("page" => $page))) ?>" class="pk_page_navigation_number"><?php echo $page ?></a>
       <?php endif; ?>
     <?php endforeach; ?>
-  <?php endif ?>
-	<?php if ($pager->getPage() >= $pager->getLastPage()):?>
-	  <span class="pk_pager_navigation_image pk_pager_navigation_next pk_pager_navigation_disabled">Next Page</span>
-		<span class="pk_pager_navigation_image pk_pager_navigation_last pk_pager_navigation_disabled">Last Page</span>	
-	<?php else: ?>
-	  <a href="<?php echo url_for(pkUrl::addParams($pagerUrl, array("page" => $pager->getNextPage()))) ?>" class="pk_pager_navigation_image pk_pager_navigation_next">Next Page</a>
-  	<a href="<?php echo url_for(pkUrl::addParams($pagerUrl, array("page" => $pager->getLastPage()))) ?>" class="pk_pager_navigation_image pk_pager_navigation_last">Last Page</a>
-	<?php endif ?>
-</div>
+  	<?php if ($pager->getPage() >= $pager->getLastPage()):?>
+  	  <span class="pk_pager_navigation_image pk_pager_navigation_next pk_pager_navigation_disabled">Next Page</span>
+  		<span class="pk_pager_navigation_image pk_pager_navigation_last pk_pager_navigation_disabled">Last Page</span>	
+  	<?php else: ?>
+  	  <a href="<?php echo url_for(pkUrl::addParams($pagerUrl, array("page" => $pager->getNextPage()))) ?>" class="pk_pager_navigation_image pk_pager_navigation_next">Next Page</a>
+    	<a href="<?php echo url_for(pkUrl::addParams($pagerUrl, array("page" => $pager->getLastPage()))) ?>" class="pk_pager_navigation_image pk_pager_navigation_last">Last Page</a>
+  	<?php endif ?>
+  </div>
+<?php endif ?>
