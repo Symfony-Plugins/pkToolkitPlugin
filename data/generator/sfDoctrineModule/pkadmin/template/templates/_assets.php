c58ff2e19c6a37fdffb374a8bed7cab7feb35d30
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-06-08 13:45:05 -0500

    Admin generator theme CSS path wrong
    git-svn-id: http://svn.symfony-project.com/plugins/pkToolkitPlugin/trunk@19035 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfDoctrineModule/pkadmin/template/templates/_assets.php
+++ data/generator/sfDoctrineModule/pkadmin/template/templates/_assets.php
@@ -5,8 +5,8 @@
 <?php else: ?> 
 [?php slot('body_class') ?]pk-admin [?php echo $sf_params->get('action'); ?] [?php end_slot() ?]
 
-[?php use_stylesheet('/pkAdminPlugin/css/pkAdmin.css', 'first') #Admin Styles ?]
 [?php use_stylesheet('/pkContextCMSPlugin/css/pkContextCMS.css', 'first') #Temporarily For Layout ?]
+[?php use_stylesheet('/pkToolkitPlugin/css/pkAdmin.css', 'first') #Admin Styles ?]
 
 [?php use_stylesheet('/pkAdminPlugin/js/theme/ui.all.css', 'first') # JQ Date Picker Styles (This doesn't have to be the ui.all.css, we could make a custom css later ) ?]
 [?php use_javascript('/pkAdminPlugin/js/jquery-ui-personalized-1.6rc6.min.js', 'last') # JQ Date Picker JS (This can/should be consolidated with sfJqueryReloadedPlugin/js/jquery-ui-sortable...) ?]
