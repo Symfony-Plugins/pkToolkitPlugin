166beacfced1757f7df6e1131f9d42b161f9ecce
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-06-11 14:08:25 -0500

    pkAdminPlugin assets and pk-login css
    git-svn-id: http://svn.symfony-project.com/plugins/pkToolkitPlugin/trunk@19175 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfDoctrineModule/pkadmin/template/templates/_assets.php
+++ data/generator/sfDoctrineModule/pkadmin/template/templates/_assets.php
@@ -8,8 +8,8 @@
 [?php use_stylesheet('/pkToolkitPlugin/css/pkToolkit.css', 'first') ?]
 [?php use_stylesheet('/pkToolkitPlugin/css/pkAdmin.css', 'first') #Admin Styles ?]
 
-[?php use_javascript('/pkToolkitPlugin/js/pkControls.js',) ?]
-[?php use_javascript('/pkToolkitPlugin/js/pkUI.js',) ?]
+[?php use_javascript('/pkToolkitPlugin/js/pkControls.js') ?]
+[?php use_javascript('/pkToolkitPlugin/js/pkUI.js') ?]
 
 [?php use_stylesheet('/pkAdminPlugin/js/theme/ui.all.css', 'first') # JQ Date Picker Styles (This doesn't have to be the ui.all.css, we could make a custom css later ) ?]
 [?php use_javascript('/pkAdminPlugin/js/jquery-ui-personalized-1.6rc6.min.js', 'last') # JQ Date Picker JS (This can/should be consolidated with sfJqueryReloadedPlugin/js/jquery-ui-sortable...) ?]
