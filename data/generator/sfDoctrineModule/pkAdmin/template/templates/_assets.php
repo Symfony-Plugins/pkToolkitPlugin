0d6c733a438f9f55f7397f79d8b7a0db6900e4fa
agilbert <agilbert@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-06-30 13:08:36 -0500

    Needed jQuery UI for the rich date picker widget.
    git-svn-id: http://svn.symfony-project.com/plugins/pkToolkitPlugin/trunk@19730 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfDoctrineModule/pkAdmin/template/templates/_assets.php
+++ data/generator/sfDoctrineModule/pkAdmin/template/templates/_assets.php
@@ -12,6 +12,6 @@
 [?php use_javascript('/pkToolkitPlugin/js/pkControls.js') ?]
 [?php use_javascript('/pkToolkitPlugin/js/pkUI.js') ?]
 
-[?php use_stylesheet('/pkAdminPlugin/js/theme/ui.all.css', 'first') # JQ Date Picker Styles (This doesn't have to be the ui.all.css, we could make a custom css later ) ?]
-[?php use_javascript('/pkAdminPlugin/js/jquery-ui-personalized-1.6rc6.min.js', 'last') # JQ Date Picker JS (This can/should be consolidated with sfJqueryReloadedPlugin/js/jquery-ui-sortable...) ?]
+[?php use_stylesheet('/pkToolkitPlugin/css/ui-lightness/jquery-ui-1.7.2.custom.css', 'first') # JQ Date Picker Styles (This doesn't have to be the ui.all.css, we could make a custom css later ) ?]
+[?php use_javascript('/pkToolkitPlugin/js/jquery-ui-1.7.2.custom.min.js', 'last') # JQ Date Picker JS (This can/should be consolidated with sfJqueryReloadedPlugin/js/jquery-ui-sortable...) ?]
 <?php endif; ?>
