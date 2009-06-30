2a75bda15ba68fc1d0a2227f839e0efb40b1c322
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-06-30 16:26:07 -0500

    filters slidetoggle is back
    git-svn-id: http://svn.symfony-project.com/plugins/pkToolkitPlugin/trunk@19760 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfDoctrineModule/pkAdmin/template/templates/indexSuccess.php
+++ data/generator/sfDoctrineModule/pkAdmin/template/templates/indexSuccess.php
@@ -9,7 +9,7 @@
 		[?php include_partial('<?php echo $this->getModuleName() ?>/list_header', array('pager' => $pager)) ?]
 		<ul class="pk-controls pk-admin-action-controls">
 			<?php if ($this->configuration->hasFilterForm()): ?>
-  			<li class="filters">[?php echo jq_link_to_function("Filters", "$('#pk-admin-filters-container').fadeIn()" ,array('class' => 'pk-btn icon pk-settings', 'title'=>'Filter Data')) ?]</li>
+  			<li class="filters">[?php echo jq_link_to_function("Filters", "$('#pk-admin-filters-container').slideToggle()" ,array('class' => 'pk-btn icon pk-settings', 'title'=>'Filter Data')) ?]</li>
 			<?php endif; ?>
     	[?php include_partial('<?php echo $this->getModuleName() ?>/list_actions', array('helper' => $helper)) ?]		
 		</ul>
