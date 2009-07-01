1535c805574c572635e27a9671794cd9791fe7da
rickybanister <rickybanister@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-07-01 13:22:28 -0500

    prelim filter styles
    git-svn-id: http://svn.symfony-project.com/plugins/pkToolkitPlugin/trunk@19801 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfDoctrineModule/pkAdmin/template/templates/_filters.php
+++ data/generator/sfDoctrineModule/pkAdmin/template/templates/_filters.php
@@ -3,37 +3,39 @@
 
 <div id="pk-admin-filters-container">
 
-	<h3>Filters</h3>
+	  <form action="[?php echo url_for('<?php echo $this->getUrlForAction('collection') ?>', array('action' => 'filter')) ?]" method="post" id="pk-admin-filters-form">
+		
+			<h3>Filters</h3>
 
-  [?php if ($form->hasGlobalErrors()): ?]
-    [?php echo $form->renderGlobalErrors() ?]
-  [?php endif; ?]
+		  [?php if ($form->hasGlobalErrors()): ?]
+		    [?php echo $form->renderGlobalErrors() ?]
+		  [?php endif; ?]
+		
+	    <table cellspacing="0">
+	      <tfoot>
+	        <tr>
+	          <td colspan="2">
+	            [?php echo $form->renderHiddenFields() ?]
+							[?php echo jq_link_to_function('Filter<span></span>', '$("#pk-admin-filters-form").submit();', array('class' => 'pk-btn', )) ?]
+							[?php echo link_to(__('reset', array(), 'pk-admin'), '<?php echo $this->getUrlForAction('collection') ?>', array('action' => 'filter'), array('query_string' => '_reset', 'method' => 'post', 'class' => 'pk-btn icon pk-cancel event-default')) ?]
+	          </td>
+	        </tr>
+	      </tfoot>
+	      <tbody>
+	        [?php foreach ($configuration->getFormFilterFields($form) as $name => $field): ?]
+	        [?php if ((isset($form[$name]) && $form[$name]->isHidden()) || (!isset($form[$name]) && $field->isReal())) continue ?]
+	          [?php include_partial('<?php echo $this->getModuleName() ?>/filters_field', array(
+	            'name'       => $name,
+	            'attributes' => $field->getConfig('attributes', array()),
+	            'label'      => $field->getConfig('label'),
+	            'help'       => $field->getConfig('help'),
+	            'form'       => $form,
+	            'field'      => $field,
+	            'class'      => 'pk-form-row pk-admin-'.strtolower($field->getType()).' pk-admin-filter-field-'.$name,
+	          )) ?]
+	        [?php endforeach; ?]
+	      </tbody>
+	    </table>
+	  </form>
 
-  <form action="[?php echo url_for('<?php echo $this->getUrlForAction('collection') ?>', array('action' => 'filter')) ?]" method="post" id="pk-admin-filters-form">
-    <table cellspacing="0">
-      <tfoot>
-        <tr>
-          <td colspan="2">
-            [?php echo $form->renderHiddenFields() ?]
-						[?php echo jq_link_to_function('Filter<span></span>', '$("#pk-admin-filters-form").submit();', array('class' => 'pk-btn', )) ?]
-						[?php echo link_to(__('reset', array(), 'pk-admin'), '<?php echo $this->getUrlForAction('collection') ?>', array('action' => 'filter'), array('query_string' => '_reset', 'method' => 'post', 'class' => 'pk-btn icon pk-cancel event-default')) ?]
-          </td>
-        </tr>
-      </tfoot>
-      <tbody>
-        [?php foreach ($configuration->getFormFilterFields($form) as $name => $field): ?]
-        [?php if ((isset($form[$name]) && $form[$name]->isHidden()) || (!isset($form[$name]) && $field->isReal())) continue ?]
-          [?php include_partial('<?php echo $this->getModuleName() ?>/filters_field', array(
-            'name'       => $name,
-            'attributes' => $field->getConfig('attributes', array()),
-            'label'      => $field->getConfig('label'),
-            'help'       => $field->getConfig('help'),
-            'form'       => $form,
-            'field'      => $field,
-            'class'      => 'pk-form-row pk-admin-'.strtolower($field->getType()).' pk-admin-filter-field-'.$name,
-          )) ?]
-        [?php endforeach; ?]
-      </tbody>
-    </table>
-  </form>
 </div>
