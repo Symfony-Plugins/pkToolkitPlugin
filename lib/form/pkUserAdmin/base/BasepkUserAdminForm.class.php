05265eaba84d396ae6258391a6dd8ce4470c64ee
agilbert <agilbert@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-07-24 09:55:06 -0500

    Unset the is_super_admin field.
    git-svn-id: http://svn.symfony-project.com/plugins/pkToolkitPlugin/trunk@20464 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/form/pkUserAdmin/base/BasepkUserAdminForm.class.php
+++ lib/form/pkUserAdmin/base/BasepkUserAdminForm.class.php
@@ -5,6 +5,8 @@ class BasepkUserAdminForm extends sfGuardUserAdminForm
   public function configure()
   {
     parent::configure();
+    
+    unset($this['is_super_admin']);
 
     $this->setWidget('groups_list', new sfWidgetFormDoctrineChoice(array(
       'model' => 'sfGuardGroup',
