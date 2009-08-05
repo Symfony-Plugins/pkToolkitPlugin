35ccf673bc1c3ebd28bce2a619039c4993a74c2c
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-08-05 09:14:44 -0500

    Removed alerts
    git-svn-id: http://svn.symfony-project.com/plugins/pkToolkitPlugin/trunk@20806 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/PkDialogHelper.php
+++ lib/helper/PkDialogHelper.php
@@ -60,8 +60,7 @@ function pk_remote_dialog_toggle($options)
   									 $('#$id-button-close').show();" .
   									 (isset($chadFrom) ?
       							   "var arrowPosition = parseInt($('$chadFrom').offset().left);
-      								 $('#$id .pk-chad').css('left',arrowPosition+'px'); alert('hello'); /*This doesn't work Tom!*/"
-      								 : "") . "
+      								 $('#$id .pk-chad').css('left',arrowPosition+'px'); " : "") . "
   									 pkUI('#$id');
   									$('.pk-page-overlay').show();",
     ), array(
