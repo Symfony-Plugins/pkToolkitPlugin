f9205661a7246eb913104b3318172380d50df11b
agilbert <agilbert@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-08-05 11:50:59 -0500

    Removed some html comments that were cluttering markup.
    git-svn-id: http://svn.symfony-project.com/plugins/pkToolkitPlugin/trunk@20808 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/widget/sfWidgetFormFormatterPkAdmin.class.php
+++ lib/widget/sfWidgetFormFormatterPkAdmin.class.php
@@ -3,8 +3,8 @@
 class sfWidgetFormSchemaFormatterPkAdmin extends sfWidgetFormSchemaFormatter 
 {
   protected
-    $rowFormat = "<div class=\"pk-form-row\">\n  %label%\n  %field% <div class='pk-form-error'>%error%</div>\n %help%%hidden_fields%\n</div><!--/pk-form-row-->\n",
+    $rowFormat = "<div class=\"pk-form-row\">\n  %label%\n  %field% <div class='pk-form-error'>%error%</div>\n %help%%hidden_fields%\n</div>\n",
     $errorRowFormat = '%errors%',
     $helpFormat = '%help%',
-    $decoratorFormat ="<div class=\"pk-admin-form-container\">\n %content%\n</div><!--/pk-form-container-->";
+    $decoratorFormat ="<div class=\"pk-admin-form-container\">\n %content%\n</div>";
 }
