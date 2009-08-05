b9040ee0ece6d569252b9587ad96921f8e4be4ca
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-08-05 17:53:06 -0500

    added a field wrapper around the field
    git-svn-id: http://svn.symfony-project.com/plugins/pkToolkitPlugin/trunk@20836 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/widget/sfWidgetFormFormatterPkAdmin.class.php
+++ lib/widget/sfWidgetFormFormatterPkAdmin.class.php
@@ -3,7 +3,7 @@
 class sfWidgetFormSchemaFormatterPkAdmin extends sfWidgetFormSchemaFormatter 
 {
   protected
-    $rowFormat = "<div class=\"pk-form-row\">\n  %label%\n  %field% <div class='pk-form-error'>%error%</div>\n %help%%hidden_fields%\n</div>\n",
+    $rowFormat = "<div class=\"pk-form-row\">\n  %label%\n  <div class=\"pk-form-field\">%field%</div> <div class='pk-form-error'>%error%</div>\n %help%%hidden_fields%\n</div>\n",
     $errorRowFormat = '%errors%',
     $helpFormat = '%help%',
     $decoratorFormat ="<div class=\"pk-admin-form-container\">\n %content%\n</div>";
