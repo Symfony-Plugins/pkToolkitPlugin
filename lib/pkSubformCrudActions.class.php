880025cbe2655c6f19646a92632dd1294325a5bf
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-08-07 15:14:43 -0500

    // A typical Doctrine route collection CRUD action class framework, with the addition of support for subforms that // edit subsets of the object's fields via AJAX. Note that the name of your module determines the name of the // variables passed to templates eg $this->profile and $this->profile_list if the module is called profile // // TODO: configurable list of valid subforms (although the existence of the class is // a good first pass at that), with credentials and/or privileges required; pass that info to the // show partial so it can do a good job of displaying edit buttons you have privs for
    git-svn-id: http://svn.symfony-project.com/plugins/pkToolkitPlugin/trunk@20918 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/pkSubformCrudActions.class.php
@@ -0,0 +1,87 @@
+<?php
+
+// A typical Doctrine route collection CRUD action class framework, with the addition of support for subforms that
+// edit subsets of the object's fields via AJAX. Note that the name of your module determines the name of the
+// variable TODO: a list of allowed subforms (although the existence of the class is
+// a good first pass at that); ways to check credentials and privileges on subforms, exposed in such a way that
+// the show action can check them when creating edit buttons.
+
+class pkSubformCrudActions extends sfActions
+{
+  protected $module;
+  protected $singular;
+  protected $list;
+  
+  public function initialize($context, $moduleName, $actionName)
+  {
+    parent::initialize($context, $moduleName, $actionName);
+    $this->module = $moduleName;
+    $this->singular = strtolower(substr($this->module, 0, 1)) . substr($this->module, 1);
+    $this->list = $this->module . "_list";
+  }
+  
+  public function executeIndex(sfWebRequest $request)
+  {
+    $list = $this->list;
+    $this->$list = $this->getRoute()->getObjects();
+  }
+
+  public function executeShow(sfWebRequest $request)
+  {
+    $singular = $this->singular;
+    $this->$singular = $this->getRoute()->getObject();
+  }
+
+  public function executeEdit(sfWebRequest $request)
+  {
+    $this->getForm($request);
+    
+    return 'Ajax';
+  }
+  
+  public function executeUpdate(sfWebRequest $request)
+  {
+    $this->getForm($request);
+    
+    if ($this->processForm($request, $this->form))
+    {
+      return $this->renderPartial($this->module . '/' . $this->form->type);
+    }
+
+    $this->setTemplate('edit');
+
+    return 'Ajax';
+  }
+
+  public function executeDelete(sfWebRequest $request)
+  {
+    $request->checkCSRFProtection();
+
+    $this->getRoute()->getObject()->delete();
+
+    $this->redirect($this->module . '/' . 'index');
+  }
+
+  protected function processForm(sfWebRequest $request, sfForm $form)
+  {
+    $form->bind($request->getParameter($form->getName()));
+    if ($form->isValid())
+    {
+      $singular = $this->singular;
+      $this->$singular = $form->save();
+      
+      return true;
+    }
+    
+    return false;
+  }
+  
+  protected function getForm($request)
+  {
+    if ($request->hasParameter('form'))
+    {
+      $class = ucfirst($this->singular) . ucfirst($request->getParameter('form')) . 'Form';
+      $this->form = new $class($this->getRoute()->getObject());
+    }
+  }
+}
