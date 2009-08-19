e73017b4e1e34e33a54fc45950acd78a08abc158
johnnyoffline <johnnyoffline@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-08-19 16:36:32 -0500

    help text needed a wrapper
    git-svn-id: http://svn.symfony-project.com/plugins/pkToolkitPlugin/trunk@21264 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/widget/sfWidgetFormFormatterPkAdmin.class.php
+++ lib/widget/sfWidgetFormFormatterPkAdmin.class.php
@@ -5,6 +5,6 @@ class sfWidgetFormSchemaFormatterPkAdmin extends sfWidgetFormSchemaFormatter
   protected
     $rowFormat = "<div class=\"pk-form-row\">\n  %label%\n  <div class=\"pk-form-field\">%field%</div> <div class='pk-form-error'>%error%</div>\n %help%%hidden_fields%\n</div>\n",
     $errorRowFormat = '%errors%',
-    $helpFormat = '%help%',
+    $helpFormat = '<div class="pk-form-help-text">%help%</div>',
     $decoratorFormat ="<div class=\"pk-admin-form-container\">\n %content%\n</div>";
 }
