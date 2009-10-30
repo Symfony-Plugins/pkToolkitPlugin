4139fa2d07e5a35d633ffe3d8056648244f9feb5
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-10-30 16:02:15 -0500

    Hid pktaskuser from user admin module because nothing about it is appropriate to edit, this account is reserved for symfony tasks
    git-svn-id: http://svn.symfony-project.com/plugins/pkToolkitPlugin/trunk@23476 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/pkUserAdmin/actions/actions.class.php
+++ modules/pkUserAdmin/actions/actions.class.php
@@ -14,4 +14,14 @@ require_once dirname(__FILE__).'/../lib/pkUserAdminGeneratorHelper.class.php';
  */
 class pkUserAdminActions extends BasepkUserAdminActions
 {
+  protected function buildQuery()
+  {
+    $query = parent::buildQuery();
+    // This user is for running scheduled tasks only. It must remain a superuser and
+    // should never be marked active or have a known password (it has a randomly generated
+    // password just in case someone somehow marks it active). So we hide it from 
+    // the admin panel, where nothing good could ever happen to it.
+    $query->andWhere($query->getRootAlias() . '.username <> ?', array('pktaskuser'));
+    return $query;
+  }
 }
